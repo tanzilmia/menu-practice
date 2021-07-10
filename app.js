eeaab02f84e21790a1222ee7ee9d9b0ff453ae36
Md tanzil <ziltan590@gmail.com>
2021-07-10 17:54:01 +0600

    v0.1

---
+++ app.js
@@ -0,0 +1,113 @@
+const navMenu = document.getElementById('menu-box');
+const Close = document.getElementById('nav-close');
+const Toggle = document.getElementById('nav-toggle');
+
+
+
+    Toggle.addEventListener('click',()=>{
+        navMenu.classList.add('show__menu');
+
+    })
+    Close.addEventListener('click',()=>{
+        navMenu.classList.remove('show__menu');
+
+    })
+   
+
+
+
+
+
+// const navMenu = document.getElementById('nav-menu'),
+//       navToggle = document.getElementById('nav-toggle'),
+//       navClose = document.getElementById('nav-close')
+
+
+// // menu show 
+// if(navToggle){
+//     navToggle.addEventListener('click', ()=>{
+//         navMenu.classList.add('show-menu');
+//         navToggle.style.color = 'transparent'
+
+//     })
+// }
+
+// if(navClose){
+//     navClose.addEventListener('click',()=>{
+//         navMenu.classList.remove('show-menu');
+//         navToggle.style.color = 'black';
+
+
+//     })
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
